332baf59c25e41e12ea2c67fda5fd1a294c1b08c
rodrigol <rodrigol@correo.com.uy>
2015-11-19 16:40:00 -0200

    commit con insert en solr

--- EnterpriseApplicationKEDB-ejb/src/java/com/kedb/buisiness/KnowErrorBean.java
+++ EnterpriseApplicationKEDB-ejb/src/java/com/kedb/buisiness/KnowErrorBean.java
@@ -6,8 +6,21 @@
 package com.kedb.buisiness;
 
 import com.kedb.persistence.KnowErrorDao;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.client.solrj.request.AbstractUpdateRequest;
+import org.apache.solr.client.solrj.request.ContentStreamUpdateRequest;
+import org.apache.solr.client.solrj.request.UpdateRequest;
+
+import org.apache.solr.client.solrj.response.UpdateResponse;
+import org.apache.solr.common.SolrInputDocument;
 
 /**
  *
@@ -21,8 +34,25 @@ private KnowErrorDao knowErrorDao;
 
     @Override
     public void createKnowError(String name) {
-         //TODO: validaciones,logica
+       
+        try {
+           
+        HttpSolrClient solr = new HttpSolrClient("http://localhost:8983/solr/miBase");                    
+        SolrInputDocument document = new SolrInputDocument();
+        document.addField("name", name);
+  
+        UpdateRequest req = new UpdateRequest();
+        req.setAction( UpdateRequest.ACTION.COMMIT, false, false );
+        req.add( document );
+        UpdateResponse response = req.process(solr);
+        System.out.println(response);
+       // solr.add(document);
+        solr.commit();        
+    } catch (Exception ex) {
+        Logger.getLogger(KnowErrorBean.class.getName()).log(Level.SEVERE, null, ex);    
+    }          
          knowErrorDao.createKnowError(name);
-    }
+    }         
+ }
 
-}
+
