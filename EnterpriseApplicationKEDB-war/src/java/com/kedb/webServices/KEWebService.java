76031b9131e2508c54014ce9b459c8e6950791ab
zalomartin <gmbesio@gmail.com>
2015-11-22 11:00:51 -0300

    insertando ke en mysql y solr

--- EnterpriseApplicationKEDB-war/src/java/com/kedb/webServices/KEWebService.java
+++ EnterpriseApplicationKEDB-war/src/java/com/kedb/webServices/KEWebService.java
@@ -7,31 +7,32 @@ package com.kedb.webServices;
 
 import com.kedb.buisiness.KnowErrorService;
 import javax.ejb.EJB;
+import javax.ejb.Stateful;
+import javax.ejb.Stateless;
 import javax.faces.bean.RequestScoped;
-import javax.jws.WebService;
-import javax.jws.WebMethod;
-import javax.jws.WebParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 
-
-@WebService(serviceName = "NewWebService")
+//@Stateless
+@Stateful
 @RequestScoped
 @Path("/knowErrors")
-public class NewWebService {
+public class KEWebService {
 
 @EJB
-private KnowErrorService ke;
+private KnowErrorService bLocal2;
 
-@POST
-@Path("/newKE")
-@Consumes("application/x-www-form-urlencoded")
+   @POST
+   @Path("/createKEE")
+   @Consumes("application/x-www-form-urlencoded")
+ // @Consumes(MediaType.APPLICATION_JSON)
    public String addKnowError(@FormParam("cause") String cause, @FormParam("solution") String solution, @FormParam("workaround") String workaround, @FormParam("category") String category) {
-       if (ke==null) return "error"; 
+       if (bLocal2==null) return "error"; 
        System.out.println("-----------cause"+ cause);
-       ke.createKnowError(cause, solution, workaround, category);
+       bLocal2.createKnowError(cause, solution, workaround, category);
        return "OK";
     }
 }
--- EnterpriseApplicationKEDB-war/src/java/com/kedb/webServices/KnowErrorWebService.java
+++ EnterpriseApplicationKEDB-war/src/java/com/kedb/webServices/KnowErrorWebService.java
@@ -10,29 +10,31 @@ import javax.ejb.EJB;
 import javax.ejb.Stateful;
 import javax.ejb.Stateless;
 import javax.faces.bean.RequestScoped;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
 
-//@Stateless
-@Stateful
+
+//@WebService(serviceName = "KnowErrorWebService")
 @RequestScoped
 @Path("/knowError")
 public class KnowErrorWebService {
 
 @EJB
-private KnowErrorService bLocal2;
+private KnowErrorService bLocal;
 
    @POST
    @Path("/createKE")
-   @Consumes("application/x-www-form-urlencoded")
- // @Consumes(MediaType.APPLICATION_JSON)
+   @Consumes("application/x-www-form-urlencoded") 
    public String addKnowError(@FormParam("cause") String cause, @FormParam("solution") String solution, @FormParam("workaround") String workaround, @FormParam("category") String category) {
-       if (bLocal2==null) return "error"; 
+      if (bLocal==null) return "error";
        System.out.println("-----------cause"+ cause);
-       bLocal2.createKnowError(cause, solution, workaround, category);
+       bLocal.createKnowError(cause, solution, workaround, category);
        return "OK";
     }
+   
 }
